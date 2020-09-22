47c89ef26361b12dbb6b3b62cfbfa1c430218aea
SyncEye <50116007+DamjanPetrovicki@users.noreply.github.com>
2020-09-22 09:45:05 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -9,19 +9,14 @@ version '1.3.0'
 server_scripts {
 	'@mysql-async/lib/MySQL.lua',
 	'@es_extended/locale.lua',
-	'locales/br.lua',
+	'locales/*.lua',
 	'config.lua',
 	'server/main.lua'
 }
 
 client_scripts {
 	'@es_extended/locale.lua',
-	'locales/br.lua',
+	'locales/*.lua',
 	'config.lua',
 	'client/main.lua'
-}
-
-dependencies {
-	'es_extended',
-	'esx_billing'
 }
